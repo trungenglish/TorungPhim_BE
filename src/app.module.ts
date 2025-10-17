0d8f41785f17e55a6a7258607ef4b9ea3f76415a
trungenglish <nguyentrung23232323@gmail.com>
2025-10-17 14:55:58 +0700

    chore: base db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GlobalExceptionFilter } from './common/filters/all-exception.filter';
 import { APP_FILTER } from '@nestjs/core';
@@ -7,30 +12,30 @@ import { UsersModule } from './modules/users/users.module';
 import { winstonConfig } from './config/wiston.config';
 import { WinstonModule } from 'nest-winston';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { MoviesModule } from './modules/movies/movies.module';
+import { envConfig } from './config/env.config';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      // envFilePath: '.env',
-      // cache: true,
-      // expandVariables: true,
-      // validationSchema: null,
-    }),
+    ConfigModule.forRoot(envConfig()),
     WinstonModule.forRootAsync(winstonConfig),
     AuthModule,
     UsersModule,
+    MoviesModule,
   ],
-  controllers: [],
   providers: [
     {
       provide: APP_FILTER,
       useClass: GlobalExceptionFilter,
     },
+    PrismaService,
   ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
   }
 }
