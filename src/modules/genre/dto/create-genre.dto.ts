da9d300f3da527629212cd920995c9eb179ce61c
trungenglish <nguyentrung23232323@gmail.com>
2025-10-23 16:23:23 +0700

    chore: cmt some code unused

--- src/modules/genre/dto/create-genre.dto.ts
+++ src/modules/genre/dto/create-genre.dto.ts
@@ -1,9 +1,7 @@
 import { IsString, IsNotEmpty } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateGenreDto {
   @IsString()
   @IsNotEmpty()
-
   name: string;
 }
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,9 +1,6 @@
 import {
   Controller,
   Get,
-  Post,
-  Body,
-  Patch,
   Param,
   Delete,
 } from '@nestjs/common';
