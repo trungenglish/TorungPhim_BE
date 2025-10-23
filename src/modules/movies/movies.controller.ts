62db3020c4c45f597ba6486ba92895ffd8d5c1c9
trungenglish <nguyentrung23232323@gmail.com>
2025-10-23 16:19:44 +0700

    chore: cmt some code unused

--- src/modules/movies/movies.controller.ts
+++ src/modules/movies/movies.controller.ts
@@ -1,24 +1,22 @@
 import {
   Controller,
   Get,
-  Post,
+  // Post,
   Body,
   Patch,
   Param,
   Delete,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
-import { CreateMovieDto } from './dto/create-movie.dto';
-import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
-  @Post()
-  create(@Body() createMovieDto: CreateMovieDto) {
-    return this.moviesService.create(createMovieDto);
-  }
+  // @Post()
+  // create(@Body() createMovieDto: CreateMovieDto) {
+  //   return this.moviesService.create(createMovieDto);
+  // }
 
   @Get()
   findAll() {
@@ -30,10 +28,10 @@ export class MoviesController {
     return this.moviesService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
-    return this.moviesService.update(+id, updateMovieDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
+  //   return this.moviesService.update(+id, updateMovieDto);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
